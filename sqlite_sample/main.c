22aedcf5f5d59c8217f4e2b1ce0700cbd566126a
acolor <ycchen@mail2000.com.tw>
2015-08-24 14:30:28 +0800

    Initial Commit

---
+++ sqlite_sample/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sqlite_sample
+//
+//  Created by 陳逸青 on 2015/8/24.
+//  Copyright (c) 2015年 ___org_name___. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
